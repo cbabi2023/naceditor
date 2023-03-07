9d540ca26e1603fbdb671443dffdef4e091b7427
Abijith CB <122815590+cbabi2023@users.noreply.github.com>
2023-03-07 12:29:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,118 +1,51 @@
-const fileInput = document.querySelector(".file-input"),
-filterOptions = document.querySelectorAll(".filter button"),
-filterName = document.querySelector(".filter-info .name"),
-filterValue = document.querySelector(".filter-info .value"),
-filterSlider = document.querySelector(".slider input"),
-rotateOptions = document.querySelectorAll(".rotate button"),
-previewImg = document.querySelector(".preview-img img"),
-resetFilterBtn = document.querySelector(".reset-filter"),
-chooseImgBtn = document.querySelector(".choose-img"),
-saveImgBtn = document.querySelector(".save-img");
-
-let brightness = "100", saturation = "100", inversion = "0", grayscale = "0";
-let rotate = 0, flipHorizontal = 1, flipVertical = 1;
-
-const loadImage = () => {
-    let file = fileInput.files[0];
-    if(!file) return;
-    previewImg.src = URL.createObjectURL(file);
-    previewImg.addEventListener("load", () => {
-        resetFilterBtn.click();
-        document.querySelector(".container").classList.remove("disable");
+const uploadBox = document.querySelector(".upload-box"),
+previewImg = uploadBox.querySelector("img"),
+fileInput = uploadBox.querySelector("input"),
+widthInput = document.querySelector(".width input"),
+heightInput = document.querySelector(".height input"),
+ratioInput = document.querySelector(".ratio input"),
+qualityInput = document.querySelector(".quality input"),
+downloadBtn = document.querySelector(".download-btn");
+let ogImageRatio;
+const loadFile = (e) => {
+    const file = e.target.files[0]; // getting first user selected file
+    if(!file) return; // return if user hasn't selected any file
+    previewImg.src = URL.createObjectURL(file); // passing selected file url to preview img src
+    previewImg.addEventListener("load", () => { // once img loaded
+        widthInput.value = previewImg.naturalWidth;
+        heightInput.value = previewImg.naturalHeight;
+        ogImageRatio = previewImg.naturalWidth / previewImg.naturalHeight;
+        document.querySelector(".wrapper").classList.add("active");
     });
 }
-
-const applyFilter = () => {
-    previewImg.style.transform = `rotate(${rotate}deg) scale(${flipHorizontal}, ${flipVertical})`;
-    previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
-}
-
-filterOptions.forEach(option => {
-    option.addEventListener("click", () => {
-        document.querySelector(".active").classList.remove("active");
-        option.classList.add("active");
-        filterName.innerText = option.innerText;
-
-        if(option.id === "brightness") {
-            filterSlider.max = "200";
-            filterSlider.value = brightness;
-            filterValue.innerText = `${brightness}%`;
-        } else if(option.id === "saturation") {
-            filterSlider.max = "200";
-            filterSlider.value = saturation;
-            filterValue.innerText = `${saturation}%`
-        } else if(option.id === "inversion") {
-            filterSlider.max = "100";
-            filterSlider.value = inversion;
-            filterValue.innerText = `${inversion}%`;
-        } else {
-            filterSlider.max = "100";
-            filterSlider.value = grayscale;
-            filterValue.innerText = `${grayscale}%`;
-        }
-    });
+widthInput.addEventListener("keyup", () => {
+    // getting height according to the ratio checkbox status
+    const height = ratioInput.checked ? widthInput.value / ogImageRatio : heightInput.value;
+    heightInput.value = Math.floor(height);
 });
-
-const updateFilter = () => {
-    filterValue.innerText = `${filterSlider.value}%`;
-    const selectedFilter = document.querySelector(".filter .active");
-
-    if(selectedFilter.id === "brightness") {
-        brightness = filterSlider.value;
-    } else if(selectedFilter.id === "saturation") {
-        saturation = filterSlider.value;
-    } else if(selectedFilter.id === "inversion") {
-        inversion = filterSlider.value;
-    } else {
-        grayscale = filterSlider.value;
-    }
-    applyFilter();
-}
-
-rotateOptions.forEach(option => {
-    option.addEventListener("click", () => {
-        if(option.id === "left") {
-            rotate -= 90;
-        } else if(option.id === "right") {
-            rotate += 90;
-        } else if(option.id === "horizontal") {
-            flipHorizontal = flipHorizontal === 1 ? -1 : 1;
-        } else {
-            flipVertical = flipVertical === 1 ? -1 : 1;
-        }
-        applyFilter();
-    });
+heightInput.addEventListener("keyup", () => {
+    // getting width according to the ratio checkbox status
+    const width = ratioInput.checked ? heightInput.value * ogImageRatio : widthInput.value;
+    widthInput.value = Math.floor(width);
 });
-
-const resetFilter = () => {
-    brightness = "100"; saturation = "100"; inversion = "0"; grayscale = "0";
-    rotate = 0; flipHorizontal = 1; flipVertical = 1;
-    filterOptions[0].click();
-    applyFilter();
-}
-
-const saveImage = () => {
+const resizeAndDownload = () => {
     const canvas = document.createElement("canvas");
+    const a = document.createElement("a");
     const ctx = canvas.getContext("2d");
-    canvas.width = previewImg.naturalWidth;
-    canvas.height = previewImg.naturalHeight;
+    // if quality checkbox is checked, pass 0.5 to imgQuality else pass 1.0
+    // 1.0 is 100% quality where 0.5 is 50% of total. you can pass from 0.1 - 1.0
+    const imgQuality = qualityInput.checked ? 0.5 : 1.0;
+    // setting canvas height & width according to the input values
+    canvas.width = widthInput.value;
+    canvas.height = heightInput.value;
+    // drawing user selected image onto the canvas
+    ctx.drawImage(previewImg, 0, 0, canvas.width, canvas.height);
     
-    ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    if(rotate !== 0) {
-        ctx.rotate(rotate * Math.PI / 180);
-    }
-    ctx.scale(flipHorizontal, flipVertical);
-    ctx.drawImage(previewImg, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-    
-    const link = document.createElement("a");
-    link.download = "image.jpg";
-    link.href = canvas.toDataURL();
-    link.click();
+    // passing canvas data url as href value of <a> element
+    a.href = canvas.toDataURL("image/jpeg", imgQuality);
+    a.download = new Date().getTime(); // passing current time as download value
+    a.click(); // clicking <a> element so the file download
 }
-
-filterSlider.addEventListener("input", updateFilter);
-resetFilterBtn.addEventListener("click", resetFilter);
-saveImgBtn.addEventListener("click", saveImage);
-fileInput.addEventListener("change", loadImage);
-chooseImgBtn.addEventListener("click", () => fileInput.click());
+downloadBtn.addEventListener("click", resizeAndDownload);
+fileInput.addEventListener("change", loadFile);
+uploadBox.addEventListener("click", () => fileInput.click());
